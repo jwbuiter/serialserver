05d43c098cc09150a27b0e9e58768db9a1552e3b
jwbuiter <jwbuiter@gmail.com>
2018-01-31 11:09:45 +0000

    hopefully improved installation script

--- install.sh
+++ install.sh
@@ -7,4 +7,8 @@ apt-get install -y minicom
 cd ..
 rm -rf tmp
 npm install
-
+adduser --disabled-password --gecos "" serialserver
+chown -R serialserver:serialserver /srv/serialserver
+[ -e /etc/systemd/system/serialserver.service ] && rm /etc/systemd/system/serialserver.service
+cp serialserver.service /etc/systemd/system/
+systemctl enable flood
