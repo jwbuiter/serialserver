967a0547826fbd11c898f41bd62969a084a0dec4
jwbuiter <jwbuiter@gmail.com>
2018-01-31 11:03:36 +0000

    improved installation script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-apt-get update
-apt-get upgrade -y
 mkdir tmp
 cd tmp
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
+apt-get install -y minicom
 cd ..
 rm -rf tmp
 npm install
+
