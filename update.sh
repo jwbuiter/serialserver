32d2b1c66c62d21489cb85497cb64ad9173efa1d
jwbuiter <jwbuiter@gmail.com>
2018-02-05 19:40:36 +0000

    added update script

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git reset --hard
+git pull
+npm install
+[ -e /etc/systemd/system/serialserver.service ] && rm /etc/systemd/system/serialserver.service
+cp serialserver.service /etc/systemd/system/
