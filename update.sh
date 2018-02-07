c4566d1c78264625fd155e50316e566886752161
jwbuiter <jwbuiter@gmail.com>
2018-02-07 17:11:18 +0000

    reload systemctl after update

--- update.sh
+++ update.sh
@@ -4,3 +4,5 @@ git pull
 npm install
 [ -e /etc/systemd/system/serialserver.service ] && rm /etc/systemd/system/serialserver.service
 cp serialserver.service /etc/systemd/system/
+systemctl daemon-reload
+
