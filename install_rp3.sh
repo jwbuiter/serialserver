d56c6741b324a9c97bf553d8571ac9286b0f1131
Jan Buiter <jwbuiter@gmail.com>
2024-02-03 15:29:37 +0100

    add install script for rp3

---
+++ install_rp3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+git clone https://github.com/jwbuiter/serialserverclient2.git --depth 1
+mv serialserverclient2 client2
+mkdir tmp
+cd tmp
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+apt-get install -y nodejs 
+cd ..
+rm -rf tmp
+npm install --unsafe-perm
+npm run build
+adduser --disabled-password --gecos "" serialserver
+chown -R serialserver:serialserver /srv/serialserver
+chmod 777 /srv/serialserver
+/bin/cp services/* /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable serialserver
+systemctl enable ssh
+systemctl start ssh
+
+if [ -f "config.static.json" ]
+then
+	echo "static config already exists."
+else
+  cp config.static.template.json config.static.json
+	echo "copying static config"
+fi
